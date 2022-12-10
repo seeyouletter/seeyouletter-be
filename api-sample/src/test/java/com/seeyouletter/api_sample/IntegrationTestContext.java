a0f4a2682ca393053aaf141bd268a7108673dfbd
sinbom <dev.sinbom@gmail.com>
2022-12-10 15:49:46 +0900

    chore: 로컬, 테스트 환경 인프라 localstack, aws 설정
    - application.yml profile 분리
    - aws s3 설정
    - 로컬 환경 인프라 docker-compose 수정(localstack 추가)
    - api module integration test context 추가

--- api-sample/src/test/java/com/seeyouletter/api_sample/IntegrationTestContext.java
+++ api-sample/src/test/java/com/seeyouletter/api_sample/IntegrationTestContext.java
@@ -6,9 +6,15 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+
+import java.nio.file.Paths;
+
+import static org.testcontainers.containers.BindMode.READ_ONLY;
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.S3;
 
 @Disabled
 @SpringBootTest
@@ -19,16 +25,45 @@ public abstract class IntegrationTestContext {
     @Container
     private static final MongoDBContainer MONGODB_CONTAINER;
 
+    @Container
+    public static final LocalStackContainer LOCAL_STACK_CONTAINER;
+
     private static final String MONGODB_VERSION = "5.0.14";
 
     private static final String MONGODB_IMAGE = "mongo";
 
+    private static final String LOCAL_STACK_VERSION = "0.11.3";
+
+    private static final String LOCAL_STACK_IMAGE = "localstack/localstack";
+
     static {
         MONGODB_CONTAINER = createMongoDBContainer();
+        LOCAL_STACK_CONTAINER = createLocalStackContainer();
     }
 
     private static MongoDBContainer createMongoDBContainer() {
         return new MongoDBContainer(createDockerImageName());
+    }
+
+    private static LocalStackContainer createLocalStackContainer() {
+        return new LocalStackContainer(createLocalStackImageName())
+                .withServices(S3)
+                .withFileSystemBind(
+                        parseLocalStackInitScriptPath(),
+                        "/docker-entrypoint-initaws.d",
+                        READ_ONLY
+                );
+    }
+
+    private static String parseLocalStackInitScriptPath() {
+        return Paths
+                .get("")
+                .toAbsolutePath()
+                .getParent()
+                .resolve("docker")
+                .resolve("localstack")
+                .normalize()
+                .toString();
     }
 
     private static DockerImageName createDockerImageName() {
@@ -37,9 +72,16 @@ public abstract class IntegrationTestContext {
                 .withTag(MONGODB_VERSION);
     }
 
+    private static DockerImageName createLocalStackImageName() {
+        return DockerImageName
+                .parse(LOCAL_STACK_IMAGE)
+                .withTag(LOCAL_STACK_VERSION);
+    }
+
     @DynamicPropertySource
     static void registerDynamicProperty(DynamicPropertyRegistry registry) {
         registry.add("spring.data.mongodb.uri", MONGODB_CONTAINER::getReplicaSetUrl);
+        registry.add("aws.endpoint", () -> LOCAL_STACK_CONTAINER.getEndpointOverride(S3).toString());
     }
 
 }
---
+++ docker/localstack/s3-init.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Init localstack"
+awslocal s3 mb s3://seeyouletter
+echo "Init localstack finished"
