023235b8f6ee27525b841491c13b49c0629c9102
Msukoo <rnalsttnn@gmail.com>
2022-12-11 18:18:39 +0900

    add: [module] sample to member

---
+++ api-member/src/main/java/com/seeyouletter/api_member/ApiMemberApplication.java
@@ -0,0 +1,12 @@
+package com.seeyouletter.api_member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMemberApplication.class, args);
+    }
+}
---
+++ api-member/src/test/java/com/seeyouletter/api_member/ApiMemberApplicationTest.java
@@ -0,0 +1,15 @@
+package com.seeyouletter.api_member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMemberApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+
+}
