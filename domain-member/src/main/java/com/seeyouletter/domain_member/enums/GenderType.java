d12e3efc96c81e9ffccbfe259e46a4f9fb6c7d4c
Msukoo <rnalsttnn@gmail.com>
2022-12-26 18:37:57 +0900

    feat(domain-member): 성별을 관리 할 수 있는 Enum 및 Converter 추가

---
+++ domain-member/src/main/java/com/seeyouletter/domain_member/enums/GenderType.java
@@ -0,0 +1,22 @@
+package com.seeyouletter.domain_member.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public enum GenderType {
+    MALE("M"),
+    FEMALE("F");
+
+    private final String type;
+
+    public static GenderType find(String code){
+        return Arrays.stream(GenderType.values())
+                .filter(x->x.getType().equals(code))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("잘못된 성별타입 입니다."));
+    }
+}
