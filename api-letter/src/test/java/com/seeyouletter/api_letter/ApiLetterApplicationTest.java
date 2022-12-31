2d1291a3273be7c4cd222313192ef9090bf9b7a9
sinbom <dev.sinbom@gmail.com>
2022-12-31 16:59:25 +0900

    chore(configuration): application.yml 불필요한 property 제거

--- api-letter/src/test/java/com/seeyouletter/api_letter/ApiLetterApplicationTest.java
+++ api-letter/src/test/java/com/seeyouletter/api_letter/ApiLetterApplicationTest.java
@@ -2,8 +2,6 @@ package com.seeyouletter.api_letter;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-
 class ApiLetterApplicationTest extends IntegrationTestContext {
 
     @Test
--- api-member/src/test/java/com/seeyouletter/api_member/ApiMemberApplicationTest.java
+++ api-member/src/test/java/com/seeyouletter/api_member/ApiMemberApplicationTest.java
@@ -2,8 +2,10 @@ package com.seeyouletter.api_member;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "test")
 class ApiMemberApplicationTest {
 
     @Test
