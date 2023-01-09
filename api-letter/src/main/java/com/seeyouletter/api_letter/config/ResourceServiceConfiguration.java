a5f6317ebda2abe53041156d60d916d1f7f5324e
Youngjin Sin <dev.sinbom@gmail.com>
2023-01-09 16:19:20 +0900

    feat(configuration): oauth2 resource server configuration 추가 (#22)
    - spring-boot-starter-oauth2-resource-server dependency 추가
    - oauth2 resource server filter chain 추가
    - resource server의 REST API 접근 권한을 method security 방식으로 인가를 처리하도록 적용

---
+++ api-letter/src/main/java/com/seeyouletter/api_letter/config/ResourceServiceConfiguration.java
@@ -0,0 +1,23 @@
+package com.seeyouletter.api_letter.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServiceConfiguration {
+
+    @Bean
+    public SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .requestMatchers()
+                .mvcMatchers("/api/**")
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return httpSecurity.build();
+    }
+
+}
