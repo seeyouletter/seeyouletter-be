3d9b594cb45b34fdf8f8e31cf7da3c404a8a9118
Msukoo <rnalsttnn@gmail.com>
2023-01-23 14:42:06 +0900

    build(api-member): jasyptt encrypt key 환경변수 처리, github action 스크립트에도 적용

--- api-member/src/main/java/com/seeyouletter/api_member/config/JasyptConfig.java
+++ api-member/src/main/java/com/seeyouletter/api_member/config/JasyptConfig.java
@@ -3,21 +3,22 @@ package com.seeyouletter.api_member.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password}")
-    private String encryptKey;
+//    @Value("${jasypt.encryptor.password}")
+//    private String encryptKey;
+
+    public static final String ENV_JASYPT_KEY = "ENCRYPTION_KEY";
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor(){
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(encryptKey);
+        config.setPassword(System.getenv(ENV_JASYPT_KEY));
         config.setPoolSize("1");
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setStringOutputType("base64");
