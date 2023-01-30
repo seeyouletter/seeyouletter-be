369bd06069bc17171a7496889a763710f02597ad
sinbom <dev.sinbom@gmail.com>
2023-01-30 20:49:52 +0900

    chore(cors): 로컬 및 개발 서버 cors 허용 설정 추가
    - 각 security filter chain에 cors를 허용하는 설정 추가

---
+++ api-member/src/main/java/com/seeyouletter/api_member/controller/DefaultController.java
@@ -0,0 +1,14 @@
+package com.seeyouletter.api_member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping(value = "/authorized")
+    public String authorized() {
+        return "authorized";
+    }
+
+}
