713fd0e1310f4f2d77b2c7d60e000bc7c73f2e0a
Youngjin Sin <dev.sinbom@gmail.com>
2023-02-02 22:38:23 +0900

    chore(cors): 로컬 및 개발 서버 cors 허용 설정 추가 (#34)
    - 각 security filter chain에 cors를 허용하는 설정 추가

--- api-member/src/main/java/com/seeyouletter/api_member/config/ResourceServiceConfiguration.java
+++ api-member/src/main/java/com/seeyouletter/api_member/config/ResourceServiceConfiguration.java
@@ -4,13 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServiceConfiguration {
 
     @Bean
-    public SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain resourceServerSecurityFilterChain(HttpSecurity httpSecurity,
+                                                                 CorsConfigurationSource corsConfigurationSource) throws Exception {
         httpSecurity
+                .cors()
+                .configurationSource(corsConfigurationSource)
+                .and()
                 .requestMatchers()
                 .mvcMatchers("/api/**")
                 .and()
