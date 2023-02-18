6bd836cb196e74f17f00c07eabdf6ffe66f9ff0c
Msukoo <rnalsttnn@gmail.com>
2023-02-18 15:00:19 +0900

    chore(api-member): 테스트유저 insert sql 추가

---
+++ api-member/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- 테스트유저
+-- id: email@gmail.com
+-- pw: 12345678
+INSERT INTO
+users(email, password, name, gender_type, birth, phone, reg_date )
+VALUES('email@gmail.com', '$2a$10$IAMyFV/oYs3EwmIFjYG8LuryFS2oSIKOlYw19.LRiYmwZ218uwZs6', '김레터', 'M', '1992-08-02', '01094949494', '2023-01-28 02:16:23');
